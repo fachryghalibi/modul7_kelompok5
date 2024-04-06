36331e604e14d84277f3fb0892158327629dc0d4
fachryghalibi <fachruddinghalibi@gmail.com>
2024-04-06 08:25:42 +0700

    menambahkan class DataMahasiswa

--- modul7_kelompok5/Program.cs
+++ modul7_kelompok5/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul7_kelompok5;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataMahasiswa_1302223107 mhs = new DataMahasiswa_1302223107();
+        mhs.ReadJSON();
+
+    }
+}
